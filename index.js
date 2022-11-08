fb7c1cd9908e4f3f7d6ddf369bdc169795786240
himel-shimul <himelshimul00@gmail.com>
2022-11-08 15:50:17 +0600

    database add

--- index.js
+++ index.js
@@ -9,14 +9,20 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2zjbmhw.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const serviceCollection = client.db('oliviasKitchen').collection('services');
+        
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 
