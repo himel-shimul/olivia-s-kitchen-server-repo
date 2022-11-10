33fbe4e5cf9df5d743f79865dfa15f59feba7d21
himel-shimul <himelshimul00@gmail.com>
2022-11-10 11:09:54 +0600

    post reviews

--- index.js
+++ index.js
@@ -79,7 +79,12 @@ async function run(){
         })
 
         // delete reviews
-        
+        app.delete('/review/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result)
+        })
 
     }
     finally{
